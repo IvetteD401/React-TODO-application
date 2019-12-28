8520ba21bc30a4307e2bc054fefeba1d2d4394b2
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-28 01:31:31 +0000

    add style

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,40 +4,38 @@ import React from "react";
 
 //create your first component
 export function Home() {
-	var myNodelist = document.getElementsByTagName("LI");
-	var i;
-	for (i = 0; i < myNodelist.length; i++) {
-		var span = document.createElement("SPAN");
-		var txt = document.createTextNode("\u00D7");
-		span.className = "close";
-		span.appendChild(txt);
-		myNodelist[i].appendChild(span);
-	}
+	const newElement = () => {
+		var myNodelist = document.getElementsByTagName("LI");
+		var i;
+		for (i = 0; i < myNodelist.length; i++) {
+			var span = document.createElement("SPAN");
+			var txt = document.createTextNode("\u00D7");
+			span.className = "close";
+			span.appendChild(txt);
+			myNodelist[i].appendChild(span);
+		}
 
-	// Click on a close button to hide the current list item
-	var close = document.getElementsByClassName("close");
-	var i;
-	for (i = 0; i < close.length; i++) {
-		close[i].onclick = function() {
-			var div = this.parentElement;
-			div.style.display = "none";
-		};
-	}
+		// Click on a close button to hide the current list item
+		var close = document.getElementsByClassName("close");
+		var i;
+		for (i = 0; i < close.length; i++) {
+			close[i].onclick = function() {
+				var div = this.parentElement;
+				div.style.display = "none";
+			};
+		}
 
-	// Add a "checked" symbol when clicking on a list item
-	// var list = document.querySelector("ul");
-	// list.addEventListener(
-	// 	"click",
-	// 	function(ev) {
-	// 		if (ev.target.tagName === "LI") {
-	// 			ev.target.classList.toggle("checked");
-	// 		}
-	// 	},
-	// 	false
-	// );
-
-	// Create a new list item when clicking on the "Add" button
-	function newElement() {
+		// Add a "checked" symbol when clicking on a list item
+		var list = document.querySelector("ul");
+		list.addEventListener(
+			"click",
+			function(ev) {
+				if (ev.target.tagName === "LI") {
+					ev.target.classList.toggle("checked");
+				}
+			},
+			false
+		);
 		var li = document.createElement("li");
 		var inputValue = document.getElementById("myInput").value;
 		var t = document.createTextNode(inputValue);
@@ -60,24 +58,36 @@ export function Home() {
 				var div = this.parentElement;
 				div.style.display = "none";
 			};
+			newElement();
 		}
-	}
+	};
+	// div.style.display = "none";
+
 	return (
-		<div className="text-center mt-5">
-			<div>
-				<div id="myDIV" className="header">
-					<h2 style={{ margin: "5px" }}>My To Do List</h2>
-					<input type="text" id="myInput" placeholder="Title..." />
-					<span
-						onclick="newElement()"
-						type="button"
-						id="myBtn"
-						className="addBtn btn-outline-dark">
-						Add
-					</span>
+		<>
+			<div className="container text-center mt-5">
+				<div>
+					<div id="myDIV" className="header  mx-auto">
+						<h2 style={{ margin: "5px" }}>My To Do List</h2>
+						<input
+							type="text"
+							id="myInput"
+							placeholder="Title..."
+						/>
+						<span onClick="newElement()" className="addBtn">
+							Add
+						</span>
+					</div>
+					<ul id="myUL">
+						<li>Hit the gym</li>
+						<li className="checked">Pay bills</li>
+						<li>Meet George</li>
+						<li>Buy eggs</li>
+						<li>Read a book</li>
+						<li>Organize office</li>
+					</ul>
 				</div>
-				<ul id="myUL" />
 			</div>
-		</div>
+		</>
 	);
 }
